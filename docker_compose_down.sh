f7bdbad7efb14ef5ffb329ccbf11232588134d00
Marvin Simon <simon.marvin.1@web.de>
2025-07-02 12:48:29 +0200

    add down of all

---
+++ docker_compose_down.sh
@@ -0,0 +1,6 @@
+docker compose -f docker-compose.yml \
+    -f traefik/docker-compose.traefik.yml \
+    -f kitchen_owl/docker-compose.kitchenowl.yml \
+    -f actual/docker-compose.actual.yml \
+    -f immich/docker-compose.immich.yml \
+    down
