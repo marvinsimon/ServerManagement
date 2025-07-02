b8dd60ae6653af854ffffbc3cba57a338d265fc0
Marvin Simon <simon.marvin.1@web.de>
2025-07-02 12:45:06 +0200

    chore: remove unneccesary files and make bash script better

--- docker_compose_up.sh
+++ docker_compose_up.sh
@@ -1,1 +1,7 @@
-docker compose -f docker-compose.yml -f traefik/docker-compose.traefik.yml -f kitchen_owl/docker-compose.kitchenowl.yml -f actual/docker-compose.actual.yml -f immich/docker-compose.immich.yml up -d
+docker compose -f docker-compose.yml \
+    -f traefik/docker-compose.traefik.yml \
+    -f kitchen_owl/docker-compose.kitchenowl.yml \
+    -f actual/docker-compose.actual.yml \
+    -f immich/docker-compose.immich.yml \
+    --env-files immich/immich.env \
+    up -d
