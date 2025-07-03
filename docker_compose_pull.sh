5c8409dcdf5b8c632bb699132e23ae80a75d5790
Marvin Simon <simon.marvin.1@web.de>
2025-07-03 15:41:56 +0200

    chore: typo and healthchceck

--- docker_compose_pull.sh
+++ docker_compose_pull.sh
@@ -4,6 +4,6 @@ docker compose -f docker-compose.yml \
     -f actual/docker-compose.actual.yml \
     -f immich/docker-compose.immich.yml \
     -f paperless/docker-compose.paperless.yml \
-    --env-file paperless/paerless.env \
+    --env-file paperless/paperless.env \
     --env-file immich/immich.env \
     pull -d
