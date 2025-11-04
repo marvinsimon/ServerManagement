5e8fd219c80404ae3eab32f416fe1921ce07dcd0
Marvin Simon <simon.marvin.1@web.de>
2025-11-04 23:14:35 +0100

    chore: rename and add containername

--- docker_compose_down.sh
+++ docker_compose_down.sh
@@ -5,6 +5,7 @@ docker compose -f docker-compose.yml \
     -f immich/docker-compose.immich.yml \
     -f paperless/docker-compose.paperless.yml \
     -f sandbox/docker-compose.wordpress.yml \
+    -f openweb_ui/docker-compose.openweb_ui.yml \
     --env-file paperless/paperless.env \
     --env-file immich/immich.env \
     --env-file sandbox/wordpress.env \
