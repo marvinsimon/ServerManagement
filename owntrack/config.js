f7cb220dfcd2dcab46a8c5e4ed45a568440c7559
Marvin Simon <simon.marvin.1@web.de>
2026-06-23 12:53:23 +0200

    feat: add owntrack

---
+++ owntrack/config.js
@@ -0,0 +1,8 @@
+window.owntracks = window.owntracks || {};
+window.owntracks.config = {
+    api: {
+    baseUrl: "https://api.owntracks.example.com",
+  },
+   ignorePingLocation: false,
+   locale: "de-DE"
+};
