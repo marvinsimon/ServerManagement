d41841d37abfb189e41d9cf9dc9ff7ed72e0ad4f
Marvin Simon <simon.marvin.1@web.de>
2026-06-23 17:49:58 +0200

    chore: remove owntrack add dawarich

--- owntrack/config.js
+++
@@ -1,8 +0,0 @@
-window.owntracks = window.owntracks || {};
-window.owntracks.config = {
-    api: {
-    baseUrl: "https://api.owntracks.example.com",
-  },
-   ignorePingLocation: false,
-   locale: "de-DE"
-};
